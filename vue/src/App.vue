1429a6ce1b000d686afe368304bbba8a7453426b
BossKing-newbie <724574109@qq.com>
2020-11-21 20:49:04 +0800

    init project

---
+++ vue/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+}
+</script>
+
+<style lang="scss">
+  @import '~normalize.css/normalize.css';// normalize.css 样式格式化
+  @import './styles/index.scss'; // 全局自定义的css样式
+  body,html{
+    overflow-x: hidden;
+  }
+  .background-header{
+      background-size:100% 100%;
+   }
+  .banner-bg {
+    padding: 320px 80px;
+    position: relative;
+    background: url("./assets/banner4.jpg") no-repeat center;
+    background-size: cover;
+    z-index: 2;
+  }
+</style>
